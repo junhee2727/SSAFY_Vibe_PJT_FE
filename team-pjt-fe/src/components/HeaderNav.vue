70a05799e140cacc53f8cb29989fbb0aaa0721c7
junheeLee <geta221@naver.com>
2026-07-14 17:23:55 +0900

    router board/:post_number 추가

--- team-pjt-fe/src/components/HeaderNav.vue
+++ team-pjt-fe/src/components/HeaderNav.vue
@@ -3,12 +3,11 @@
 
 <template>
   <header class="topbar">
-    <router-link to="/" class="brand">Local Hub</router-link>
+    <router-link to="/" class="brand"><span class = "brand">로컬 허브</span> Local Hub</router-link>
     <nav class="site-nav">
-      <router-link to="/">관광지</router-link>
-      <router-link to="/">맛집</router-link>
-      <router-link to="/">축제/행사</router-link>
-      <router-link to="/board">커뮤니티</router-link>
+      <router-link to="/board">관광지</router-link>
+      <router-link to="/board">맛집</router-link>
+      <router-link to="/board">축제/행사</router-link>
     </nav>
   </header>
 </template>
--- team-pjt-fe/src/router/index.js
+++ team-pjt-fe/src/router/index.js
@@ -1,10 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Board from '../pages/Board.vue'
 import Main from '../pages/Main.vue'
+import BoardList from '../pages/BoardList.vue'
+import BoardPost from '../pages/BoardPost.vue'
 
 const routes = [
   { path: '/', name: 'Main', component: Main },
-  { path: '/board', name: 'Board', component: Board }
+  { path: '/board', name: 'Board', component: Board },
+  { path: '/board/:post_number', name: 'BoardPost', component: BoardPost, props:true}
 ]
 
 export default createRouter({
