e14a3f96f456b98064c384733d46eaa64222cf1d
junheeLee <geta221@naver.com>
2026-07-15 08:29:47 +0900

    Feat: 글쓰기  페이지, Api 라우터 등록

--- team-pjt-fe/src/router/index.js
+++ team-pjt-fe/src/router/index.js
@@ -1,13 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Board from '../pages/Board.vue'
 import Main from '../pages/Main.vue'
 import BoardList from '../pages/BoardList.vue'
 import BoardPost from '../pages/BoardPost.vue'
+import BoardWrite from '../pages/BoardWrite.vue'
 
 const routes = [
   { path: '/', name: 'Main', component: Main },
   { path: '/board', name: 'Board', component: BoardList },
-  { path: '/board/:post_number', name: 'BoardPost', component: BoardPost, props:true}
+  { path: '/board/:post_number', name: 'BoardPost', component: BoardPost, props:true},
+  { path: '/board/write', name: 'BoardWrite', component: BoardWrite, props:true}
 ]
 
 export default createRouter({
